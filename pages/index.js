2c90c446eb14a92ecc0f725bf731155ff123a8ce
mrsaifullah52 <mrsaifullah52@gmail.com>
2023-01-26 03:09:40 +0500

    footer has been updated

--- pages/index.js
+++ pages/index.js
@@ -138,10 +138,11 @@ export default function Home() {
       </main>
 
       <footer className="bg-[#5379F6] h-auto py-4 w-full flex gap-10 justify-content justify-center">
-        <a href="https://twitter.com/vouchdao" target="_blank" rel="noreferrer">
+        <a href="https://twitter.com/vouchdao" target="_blank" rel="noreferrer" className="transform hover:-translate-y-1 duration-300">
           <BrandTwitter size={32} strokeWidth={1} color={"white"} />
         </a>
-        <a href="https://discord.gg/6DaJqB4k" target="_blank" rel="noreferrer">
+
+        <a href="https://discord.gg/6DaJqB4k" target="_blank" rel="noreferrer" className="transform hover:-translate-y-1 duration-300">
           <BrandDiscord size={32} strokeWidth={1} color={"white"} />
         </a>
       </footer>
