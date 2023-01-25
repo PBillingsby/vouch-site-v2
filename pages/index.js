70214a70937c5895b93eae3601ce6438063a9aa0
mrsaifullah52 <mrsaifullah52@gmail.com>
2023-01-26 02:47:05 +0500

    header has been updated

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import Head from 'next/head'
-import { BrandTwitter, BrandDiscord, Code } from 'tabler-icons-react';
+import Head from "next/head";
+import { useState } from "react";
+import { BrandTwitter, BrandDiscord, Code } from "tabler-icons-react";
 
 export default function Home() {
+  const [IsNavOpen, setIsNavOpen] = useState(false);
   return (
     <>
       <Head>
@@ -11,27 +13,113 @@ export default function Home() {
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <main className="h-full">
-        <div className="bg-[#5379F6] h-[100px] content-center flex gap-10 md:gap-2 items-center w-full">
-          <img src="./logo.png" className="md:px-12 md:absolute w-1/3 md:w-1/4 md:left-10" />
-          <div className="flex gap-2 md:absolute md:p-[10rem] right-0">
-            <button className="rounded-lg p-1 md:py-2 md:px-4 md:btn bg-white text-[#5379F6] hover:bg-[#5379F6] hover:text-white hover:border hover:border-white">Get Vouched</button>
-            <button className="rounded-lg p-1 md:py-4 md:px-4 md:btn bg-white text-[#5379F6] hover:bg-[#5379F6] hover:text-white hover:border hover:border-white">Developers</button>
+        <header className="bg-[#5379F6]">
+          <div className="container mx-auto">
+            <div className="flex justify-between items-center px-3">
+              <img src="./logo.png" className="" width="200px" height="100px" />
+
+              <div
+                className={`md:w-auto w-full ml-auto md:relative absolute right-0 md:bg-transparent  bg-white top-[30%] ${
+                  IsNavOpen ? "visible" : "invisible"
+                } md:visible`}
+              >
+                <div className="w-full flex md:flex-row flex-col items-center md:gap-4 gap-1 py-2">
+                  <button
+                    className="md:rounded-md border border-transparent md:hover:border-white md:hover:bg-[#5379F6] md:hover:text-white 
+                    lg:px-4 lg:py-2 md:py-2 md:px-2 py-2 md:bg-white bg-[#5379F6] md:text-[#5379F6] text-white md:w-auto w-full"
+                  >
+                    Get Vouched
+                  </button>
+                  <button
+                    className="md:rounded-md border border-transparent md:hover:border-white md:hover:bg-[#5379F6] md:hover:text-white 
+                    lg:px-4 lg:py-2 md:py-2 md:px-2 py-2 md:bg-white bg-[#5379F6] md:text-[#5379F6] text-white md:w-auto w-full"
+                  >
+                    Developers
+                  </button>
+                </div>
+              </div>
+
+              <button
+                onClick={() => setIsNavOpen(!IsNavOpen)}
+                type="button"
+                className="hover:bg-[#fff] inline-flex items-center justify-center p-2 rounded-md  text-[#5379F6] bg-white 
+             md:hidden"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!IsNavOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
           </div>
-        </div>
+        </header>
+
         <div className="flex gap-20">
           <div className="md:w-[35rem] p-4 md:m-[6rem]">
-            <p className="text-4xl font-medium text-[#5379F6]">Decentralized Verification for Arweave Wallets.</p>
-            <p className="py-2 font-extralight">VouchDAO allows users to verify once and be trusted everywhere on the Permaweb, while allowing developers to verify them with 2 lines of code.</p>
-            <p className="py-2 font-extralight">It all starts with the new Vouch data protocol, defined by ANS-109. ANS-109 allows anyone on the permaweb to &quot;vouch&quot; for the identity & humanity of any other address. Anyone can run a Vouch server, providing attestations on Arweave about user identities.</p>
-            <p className="py-2 font-extralight">VouchDAO is a decentralized verification layer on top of the Vouch standard. It is a community that votes together on which vouching services are trustworthy.</p>
-            <p className="py-2 font-extralight">Community owned and operated.</p>
+            <p className="text-4xl font-medium text-[#5379F6]">
+              Decentralized Verification for Arweave Wallets.
+            </p>
+            <p className="py-2 font-extralight">
+              VouchDAO allows users to verify once and be trusted everywhere on
+              the Permaweb, while allowing developers to verify them with 2
+              lines of code.
+            </p>
+            <p className="py-2 font-extralight">
+              It all starts with the new Vouch data protocol, defined by
+              ANS-109. ANS-109 allows anyone on the permaweb to
+              &quot;vouch&quot; for the identity & humanity of any other
+              address. Anyone can run a Vouch server, providing attestations on
+              Arweave about user identities.
+            </p>
+            <p className="py-2 font-extralight">
+              VouchDAO is a decentralized verification layer on top of the Vouch
+              standard. It is a community that votes together on which vouching
+              services are trustworthy.
+            </p>
+            <p className="py-2 font-extralight">
+              Community owned and operated.
+            </p>
             <div className="pt-4 flex gap-2">
               <button className="btn border border-[#5379F6] hover:bg-[#5379F6] hover:text-white flex gap-2 rounded-lg py-2 px-4 btn bg-white text-[#5379F6]">
-                <div className="pt-1"><Code
-                  size={18}
-                  strokeWidth={2}
-                  className="border border-[#5379F6] rounded-full"
-                /></div>
+                <div className="pt-1">
+                  <Code
+                    size={18}
+                    strokeWidth={2}
+                    className="border border-[#5379F6] rounded-full"
+                  />
+                </div>
                 Developer Guide
               </button>
               <button className="flex gap-2 btn bg-[#5379F6] hover:bg-white hover:text-[#5379F6] border border-[#5379F6] rounded-lg py-2 px-4 btn text-white]">
@@ -40,25 +128,28 @@ export default function Home() {
               </button>
             </div>
           </div>
-          <img src="./hero-image.png" className="md:w-[677px]  order-first md:order-last" />
+          <img
+            src="./hero-image.png"
+            className="md:w-[677px]  order-first md:order-last"
+          />
         </div>
         <div className="bg-[#5379F6] h-auto py-4 w-full flex gap-10 justify-content justify-center">
-          <a href="https://twitter.com/vouchdao" target="_blank" rel="noreferrer" >
-            <BrandTwitter
-              size={32}
-              strokeWidth={1}
-              color={'white'}
-            />
+          <a
+            href="https://twitter.com/vouchdao"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BrandTwitter size={32} strokeWidth={1} color={"white"} />
           </a>
-          <a href="https://discord.gg/6DaJqB4k" target="_blank" rel="noreferrer" >
-            <BrandDiscord
-              size={32}
-              strokeWidth={1}
-              color={'white'}
-            />
+          <a
+            href="https://discord.gg/6DaJqB4k"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BrandDiscord size={32} strokeWidth={1} color={"white"} />
           </a>
         </div>
       </main>
     </>
-  )
+  );
 }
